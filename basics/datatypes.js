d83d1258d1cf4e8c0e4533e607d899014ae56c93
Manjit0111 <manjitbizople@gmail.com>
2025-02-07 05:43:39 +0000

    datatypes done

---
+++ basics/datatypes.js
@@ -0,0 +1,6 @@
+let num=123
+let name="manjit"
+let married=false
+console.log(typeof num)
+console.log(typeof name)
+console.log(typeof married)
---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+const accountId=123456
+let accountPass="12121"
+var bankName="Bob"
+
+console.log(accountId,accountPass,bankName);
+
+accountPass="987654"
+bankName="sbi"
+
+console.table([accountId,accountPass,bankName])
