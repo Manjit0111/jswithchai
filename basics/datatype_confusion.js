9728bd5fb8e51212c7203034a9893d0947d326fb
Manjit0111 <manjitbizople@gmail.com>
2025-02-07 07:01:38 +0000

    done

---
+++ basics/datatype_confusion.js
@@ -0,0 +1,17 @@
+let score=7
+console.log("original number",typeof score,score);
+let stringScore=String(score)
+console.log("conversaion into string",typeof stringScore,stringScore)
+let bool=true
+console.log("original boolean",typeof bool,bool)
+let numberBool=Number(bool)
+console.log("conversation into number",typeof numberBool,numberBool)
+let stringBool = String(bool)
+console.log("conversion into string",typeof stringBool,stringBool)
+let stringnum="77"
+console.log("original string",typeof stringScore,stringScore)
+let numstrifn= Number(stringnum)
+console.log("conversation into number",typeof numstrifn,numstrifn)
+
+let negScore=-score
+console.log(negScore-1);
