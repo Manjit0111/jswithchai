6917ab3995c71a1f91d01bf00ae7d8525d678943
Manjit0111 <manjitbizople@gmail.com>
2025-02-09 05:45:05 +0000

    datatypes done

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,6 +1,24 @@
+
+// prmitive
 let num=123
 let name="manjit"
 let married=false
 console.log(typeof num)
 console.log(typeof name)
 console.log(typeof married)
+
+//reference
+let employee=["emp1","emp2","emp3"]
+console.log(employee ,"is",typeof employee)
+
+let obj1={
+    name:"manjit",
+    id:123
+}
+console.log(obj1.id,"is",typeof obj1);
+
+let myfunc=function(){
+    console.log("This is function")
+}
+myfunc()
+console.log(typeof myfunc)
